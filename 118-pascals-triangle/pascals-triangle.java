e77769145f2b002d33d5be46a259e0b02a4cb166
vivanshjaiswal <vivanshjaiswal14@gmail.com>
2025-10-06 12:40:20 +0530

    Time: 1 ms (85.48%) | Memory: 42.2 MB (41.75%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    // Function to calculate nCr
+    public static int calculate(int n, int r) {
+        long res = 1;
+        for (int i = 0; i < r; i++) {
+            res = res * (n - i);   // multiply with decreasing n
+            res = res / (i + 1);   // divide by increasing i
+        }
+        return (int) res;
+    }
+
+    // Function to generate Pascal's Triangle
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int row = 1; row <= numRows; row++) {
+            List<Integer> tempList = new ArrayList<>();
+
+            for (int col = 1; col <= row; col++) {
+                tempList.add(calculate(row - 1, col - 1));
+            }
+
+            ans.add(tempList);
+        }
+
+        return ans;
+    }
+}
