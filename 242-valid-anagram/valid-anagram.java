0cc58574e84ef175bc08ad8d6fbf8b684fb49ea7
vivanshjaiswal <vivanshjaiswal14@gmail.com>
2025-10-10 22:54:11 +0530

    Time: 5 ms (54.48%) | Memory: 43.1 MB (83.09%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,11 +3,20 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        char[] arr1=s.toCharArray();
-        char[] arr2=t.toCharArray();
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
+        int [] count = new int [26];
+        for(int i=0;i<s.length();i++){
+            count[s.charAt(i)-'a']++;
+            count[t.charAt(i)- 'a']--;
+        }
+        for(int c: count){
+           if(c!=0) return false;
+        }
+        return true;
 
-       return Arrays.equals(arr1,arr2);      
+
+
+
+
+
     }
 }
