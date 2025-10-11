fb70bdb29444771fdc9e3484575bd6ed42f388c7
vivanshjaiswal <vivanshjaiswal14@gmail.com>
2025-10-11 22:11:23 +0530

    Time: 5 ms (9.12%) | Memory: 45.6 MB (20.56%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
@@ -1,13 +1,31 @@
 class Solution {
     public String largestOddNumber(String num) {
-        int largestOddIndex = -1;
+        int n = num.length();
+        int[] arr = new int[n];
 
-        for (int i = 0; i < num.length(); i++) {
-            if ((num.charAt(i) - '0') % 2 != 0) {
+        // Step 1: String ko int array me convert karna
+        for (int i = 0; i < n; i++) {
+            arr[i] = num.charAt(i) - '0';
+        }
+
+        // Step 2: Last digit odd check
+        if (arr[n - 1] % 2 != 0) {
+            return num; // poora number odd hai
+        }
+
+        // Step 3: Loop se largest odd index dhundna
+        int largestOddIndex = -1;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 != 0) {
                 largestOddIndex = i;
             }
         }
 
-        return largestOddIndex == -1 ? "" : num.substring(0, largestOddIndex + 1);
+        // Step 4: Result return
+        if (largestOddIndex == -1) {
+            return ""; // koi odd number nahi
+        } else {
+            return num.substring(0, largestOddIndex + 1);
+        }
     }
 }
