d3fe13ec8778f9b4c777aa762dda224877f13dd3
Vivansh Jaiswal <vivanshjaiswal14@gmail.com>
2026-02-13 14:34:10 +0530

    Time: 172 ms (7.85%) | Memory: 47.6 MB (30.67%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,19 +1,21 @@
 class Solution {
-    public boolean linear_search(int[] nums, int target) {
-        for (int num : nums) {
-            if (num == target) {
+    public boolean linearSearch(int [] nums,int target){
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
                 return true;
             }
         }
         return false;
     }
-
     public int missingNumber(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            if (!linear_search(nums, i)) {
+        //Brute Force Approach
+
+        for(int i=0;i<nums.length;i++){
+            if(!linearSearch(nums,i)){
                 return i;
             }
+
         }
         return nums.length;
-    }
+    }   
 }
